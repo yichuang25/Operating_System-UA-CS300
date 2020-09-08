05e0d951a74831e41b9ea47a6bcbc0d924ee9eb2
yhuang87 <yhuang87@crimson.ua.edu>
2020-09-08 17:32:17 -0500

    finish first.c implement

--- project1/first.c
+++ project1/first.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 int main(int argc, char* argv[]) {
     
-    printf("Hello world\n");
+    int k = 0;
+
+    pid_t pid;
+
+    while(k<=0) {
+        printf("Enter a positive number: \n");
+        scanf("%d",&k);
+        if(k <= 0) {
+            printf("Error, please enter a positive number\n");
+        }
+    }
+
+    pid = fork();
+
+    if(pid == 0) {
+        printf("Child is processing ...\n");
+        printf("%d\n",k);
+        while(k!=1) {
+            if(k%2 == 0) {
+                k = k/2;
+            }
+            else {
+                k = 3*k + 1;
+            }
+            printf("%d\n",k);
+        }
+        printf("Child processing is done\n");
+    }
+    else {
+        printf("Parent is waiting for child process...\n");
+        wait();
+        printf("parent processing is done\n");
+    }
 
     return 0;
 }
