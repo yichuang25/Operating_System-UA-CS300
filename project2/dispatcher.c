2063214ce3930039df0c9bd3b7fa6d9cbe54d7eb
yhuang87 <yhuang87@crimson.ua.edu>
2020-10-10 17:03:35 -0500

    finish cratch

--- project2/dispatcher.c
+++ project2/dispatcher.c
@@ -22,6 +22,7 @@ typedef struct process {
 
 
 typedef struct cda {
+    bool flag;
     bool sorted;
     int size;
     int cap;
@@ -41,6 +42,7 @@ process* newProcess(int arrival, int priority, int time) {
 
 CDA *newCDA() {
     CDA *cda = (CDA *) malloc (sizeof(CDA));
+    cda->flag = false;
     cda->size = 0;
     cda->front = 0;
     cda->cap =1;
@@ -60,7 +62,7 @@ void doubleCapacity(CDA *cda) {
     int old_cap = cda->cap;
     int old_front = cda->front;
     process *temp = malloc(cda->cap * sizeof(process));
-    for(int i=0;i<cda->cap;i++) {
+    for(int i=0;i<cda->size;i++) {
         copyProcess(cda->arr[(i+old_front)%old_cap],&temp[i]);
     }
     cda->cap = old_cap * 2;
@@ -85,7 +87,7 @@ void insertCDA_back(CDA *cda,process* np) {
         cda->sorted = true;
     }
     else {
-        if(cda->arr[(cda->front + cda->size - 1)%cda->cap].arrival_time > np->arrival_time) {
+        if(cda->arr[(cda->front + cda->size - 1)%cda->cap].arrival_time > np->arrival_time && cda->flag == false) {
             cda->sorted = false;
         }
     }
@@ -119,12 +121,12 @@ void removeCDAfront(CDA *cda) {
     if(cda->size <= 0) {
         return;
     }
-    if(cda->cap>1 && (cda->size-1)/cda->cap < 0.25) {
-        halfCapacity(cda);
-    }
-    int newfront = (cda->front + 1 + cda->size) % cda->cap;
+    int newfront = (cda->front + 1) % cda->cap;
     cda->front = newfront;
     cda->size--;
+    if(cda->cap>1 && (cda->size)/cda->cap < 0.25) {
+        halfCapacity(cda);
+    }
 }
 
 void insertionSort(CDA *cda) {
@@ -148,7 +150,8 @@ void printCDA(CDA *cda) {
 }
 
 
-void runprocess(process *p) {
+void startProcess(process *p) {
+    //printf("New process: %d, %d, %d\n",p->arrival_time,p->priority,p->cpu_time);
     pid_t child = fork();
     if(child == 0) {
         char **command = malloc(sizeof(char *));
@@ -161,8 +164,9 @@ void runprocess(process *p) {
     }
 }
 
-void stopprocess (process *p) {
-    kill(p->pid,SIGSTOP);
+void suspendProcess (process *p) {
+    //printf("Stop process: %d, %d, %d\n",p->arrival_time,p->priority,p->cpu_time);
+    kill(p->pid,SIGTSTP);
     waitpid(p->pid,NULL,WUNTRACED);
     if(p->priority < 3)  {
         p->priority++;
@@ -170,13 +174,28 @@ void stopprocess (process *p) {
     p->state = waiting;
 }
 
-void continueprocess(process *p) {
+void restartProcess(process *p) {
+    //printf("Restart process: %d, %d, %d\n",p->arrival_time,p->priority,p->cpu_time);
     kill(p->pid,SIGCONT);
 }
 
-void terminate(process *p) {
+void terminateProcess(process *p) {
+    //printf("Terminate process: %d, %d, %d\n",p->arrival_time,p->priority,p->cpu_time);
     kill(p->pid,SIGINT);
     waitpid(p->pid,NULL,WUNTRACED);
+}
+
+CDA *get_current_process(CDA *dq, int arrival) {
+    CDA *list = newCDA();
+    list->flag = true;
+    for(int i=0;i<dq->size;i++) {
+        process cur;
+        copyProcess(dq->arr[i],&cur);
+        if(cur.arrival_time == arrival) {
+            insertCDA_back(list,&cur);
+        }
+    }
+    return list;
 }
 
 
@@ -207,14 +226,20 @@ int main (int argc, char *argv[]) {
 
     while(fgets(line,BUFFER_SIZE,dispatch_file)) {
         int arrival, priority, cpu_time;
-        int count = sscanf(line,"%d,%d,%d",&arrival,&priority,&cpu_time);
+        int count;
+        count = sscanf(line,"%d,%d,%d",&arrival,&priority,&cpu_time);
         process *proc = newProcess(arrival,priority,cpu_time);
-        insertCDA_back(dispatch_queue,proc);
+        if(priority<0 || priority >3) {
+            printf("Invalid priority!\n");
+        }
+        else {
+            insertCDA_back(dispatch_queue,proc);
+        }
     }
     if (dispatch_queue->sorted == false) {
         insertionSort(dispatch_queue);
     }
-    printCDA(dispatch_queue);
+    //printCDA(dispatch_queue);
 
     int curr_time = 0;
     int number_process = 0;
@@ -223,13 +248,144 @@ int main (int argc, char *argv[]) {
     
     for(int i=0;i<4;i++) {
         rq[i] = newCDA();
+        rq[i]->flag = true;
     }
 
     process *currently_running = NULL;
     int sys_running = 0;
-    while(currently_running || number_process != dispatch_queue->size) {
-        
+    int left = dispatch_queue->size;
+    while(left > 0) {
+        if(curr_time == 7) {
+            printf("1\n");
+        }
+        //printf("Second %d\n",curr_time);
+        CDA *curr_process = get_current_process(dispatch_queue,curr_time);
+        //printf("%d\n",curr_process->size);
+        number_process = number_process + curr_process->size;
+        for(int i=0;i<curr_process->size;i++) {
+            process cur;
+            copyProcess(curr_process->arr[i],&cur);
+            insertCDA_back(rq[cur.priority],&cur);
+        }
+
+        //for(int i=0;i<4;i++) {
+        //    printf("\npriority queue: %d\n",i);
+        //    printCDA(rq[i]);
+        //    printf("\n");
+        //}
+
+        if(currently_running && currently_running->cpu_time == 0) {
+            terminateProcess(currently_running);
+            left--;
+            if(sys_running) {
+                sys_running = 0;
+
+            }
+            free(currently_running);
+            currently_running = NULL;
+            
+        }
+
+        if(rq[0]->size > 0 && !sys_running) { //system queue
+            if(currently_running) {
+                suspendProcess(currently_running);
+                insertCDA_back(rq[currently_running->priority],currently_running);
+                //free(currently_running);
+            }
+            process *sys_pro = malloc(sizeof(process));
+            copyProcess(rq[0]->arr[rq[0]->front],sys_pro);
+            removeCDAfront(rq[0]);
+            startProcess(sys_pro);
+           
+            if(currently_running) {
+                free(currently_running);
+            }
+            currently_running = sys_pro;
+            sys_running = 1;
+        }
+        else if(rq[1]->size > 0) { //priority 1
+            if(currently_running) {
+                suspendProcess(currently_running);
+                insertCDA_back(rq[currently_running->priority],currently_running);
+                //free(currently_running);
+            }
+            process *pone = malloc(sizeof(process));
+            copyProcess(rq[1]->arr[rq[1]->front],pone);
+            removeCDAfront(rq[1]);
+            if(pone->state == ready) {
+                startProcess(pone);
+            }
+            else {
+                restartProcess(pone);
+            }
+            
+            if(currently_running) {
+                free(currently_running);
+            }
+            currently_running = pone;
+            sys_running = 0;
+            
+        }
+        else if(rq[2]->size > 0) { // priority 2
+            if(currently_running) {
+                suspendProcess(currently_running);
+                insertCDA_back(rq[currently_running->priority],currently_running);
+                //free(currently_running);
+            }
+            process *ptwo = malloc(sizeof(process));
+            copyProcess(rq[2]->arr[rq[2]->front],ptwo);
+            removeCDAfront(rq[2]);
+            if(ptwo->state == ready) {
+                startProcess(ptwo);
+            }
+            else {
+                restartProcess(ptwo);
+            }
+            if(currently_running) {
+                free(currently_running);
+            }
+            currently_running = ptwo;
+            sys_running = 0;
+            
+        }
+        else if(rq[3]->size > 0) { //last priority
+            if(currently_running) {
+                suspendProcess(currently_running);
+                insertCDA_back(rq[currently_running->priority],currently_running);
+                //free(currently_running);
+            }
+            process *pthree = malloc(sizeof(process));
+            copyProcess(rq[3]->arr[rq[3]->front],pthree);
+            removeCDAfront(rq[3]);
+            if(pthree->state == ready) {
+                startProcess(pthree);
+            }
+            else {
+                restartProcess(pthree);
+            }
+
+            if(currently_running) {
+                free(currently_running);
+            }
+            currently_running = pthree;
+            sys_running = 0;
+            
+        }
+
+        if(currently_running) {
+            currently_running->cpu_time = currently_running->cpu_time - TIME_QUANTUM;
+        }
+
+        curr_time++;
+        free(curr_process);
+        sleep(TIME_QUANTUM);
     }
+
+    for(int i=0;i<4;i++) {
+        free(rq[i]->arr);
+    }
+    //free(dispatch_queue->arr);
+    fclose(dispatch_file);
 
     return 0;
 }
