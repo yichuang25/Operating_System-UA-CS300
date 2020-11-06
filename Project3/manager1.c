c9ab57cb449a5d0df78219c429ae71c03f9340c1
yhuang87 <yhuang87@crimson.ua.edu>
2020-11-06 13:06:57 -0600

    finish project3

--- Project3/manager1.c
+++ Project3/manager1.c
@@ -1,3 +1,11 @@
+/*
+*
+*   Yichen Huang
+*   CS-300-001
+*   11906882
+*
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
--- Project3/manager2.c
+++ Project3/manager2.c
@@ -1,3 +1,10 @@
+/*
+*
+*   Yichen Huang
+*   CS-300-001
+*   11906882
+*
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
